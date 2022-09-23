de8ab4363e14c1def2da08489be58104c825968a
veronicaglh <veronicagt.IT@gmail.com>
2022-09-23 19:38:27 +0300

    add the frames of the tkinter window

--- BMI.py
+++ BMI.py
@@ -7,3 +7,35 @@
 # by running the following command on your IDE's terminal
 # pip install pillow
 
+import math
+from tkinter import *
+from tkinter import messagebox
+from PIL import Image, ImageTk
+import threading
+
+# TKINTER - GUI
+mainWindow = Tk()
+mainWindow.geometry('736x414')
+mainWindow.rowconfigure(0, weight=1)
+mainWindow.columnconfigure(0, weight=1)
+mainWindow['background'] = '#DAF3F0'
+mainWindow.title("BMI")
+
+# The UI will have 3 different windows
+window1 = Frame(mainWindow)
+window2 = Frame(mainWindow)
+window3 = Frame(mainWindow)
+
+for frame in (window1, window2, window3):
+    frame.grid(row=0, column=0, sticky='nsew')
+    frame.config(background='#DAF3F0')
+
+
+# The function show_frame() will display the page when called.
+# For eg if page1 is given as an argument(show_frame(window1)) it will display page1
+# It will start by displaying window1
+def show_frame(frame):
+    frame.tkraise()
+
+
+show_frame(window1)
