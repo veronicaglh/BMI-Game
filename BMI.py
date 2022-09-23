fd763f1d196ebf3da19dfa9102102fd2e50cbe99
veronicaglh <veronicagt.IT@gmail.com>
2022-09-23 19:27:29 +0300

    first commit

---
+++ BMI.py
@@ -0,0 +1,9 @@
+# This program will calculate the BMI of a user
+# It also had a GUI that has been built with tkinter
+# In addition it also has a Game Mode(optional) which lets users
+# guess their BMI and the program will tell them when they are hot or cold.
+# This program does need the pillow package to work properly
+# So if you have not installed that you can install it
+# by running the following command on your IDE's terminal
+# pip install pillow
+
